2a2a9e99eb162e9ba400e3ec080ee5abafa6af0c
Anneme <ST10144453@vcconnect.edu.za>
2024-10-09 12:49:55 +0200

    Start on LoginRegisterMenu and can naviagte to login and register views

---
+++ ST10144453_PROG7312/MVVM/View-Model/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ST10144453_PROG7312.Core;
+using ST10144453_PROG7312.MVVM.View_Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ST10144453_PROG7312.MVVM.ViewMode
+{ 
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private object _currentView;
+
+        public MainWindowViewModel()
+        {
+            // Set the initial view to LoginRegisterMenu
+            CurrentView = new LoginRegisterViewModel();
+        }
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ShowHomeViewCommand => new RelayCommand(ShowHomeView);
+
+        private void ShowHomeView()
+        {
+            CurrentView = new HomeViewModel();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ ST10144453_PROG7312/MVVM/View/LoginUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST10144453_PROG7312.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for LoginUserControl.xaml
+    /// </summary>
+    public partial class LoginUserControl : UserControl
+    {
+        public LoginUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
